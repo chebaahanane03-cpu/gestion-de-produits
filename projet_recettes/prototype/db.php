34f1fb615ed325a12885f6ddd501e7b8d2f6f721
chebaahanane03-cpu <chebaahanane03@gmail.com>
2026-05-14 12:17:33 +0100

    Add files via upload
    prototype

---
+++ projet_recettes/prototype/db.php
@@ -0,0 +1,27 @@
+<?php
+
+$host="localhost";
+$dbname="gestion_produits";
+$user="root";
+$pass="";
+
+try{
+
+$pdo = new PDO(
+"mysql:host=$host;dbname=$dbname",
+$user,
+$pass
+);
+
+$pdo->setAttribute(
+PDO::ATTR_ERRMODE,
+PDO::ERRMODE_EXCEPTION
+);
+
+}catch(PDOException $e){
+
+die("Erreur : ".$e->getMessage());
+
+}
+
+?>
